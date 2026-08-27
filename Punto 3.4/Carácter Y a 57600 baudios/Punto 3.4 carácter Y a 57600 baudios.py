c3ae551b17833c0af78a440b6b1632b61576e29f
paulaquintero2811 <est.paula.vquintero@unimilitar.edu.co>
2026-08-26 20:27:21 -0500

    Add files via upload

---
+++ Punto 3.4/Carácter Y a 57600 baudios/Punto 3.4 carácter Y a 57600 baudios.py
@@ -0,0 +1,11 @@
+import machine
+import utime
+from machine import Pin, UART
+led = machine.Pin("LED", machine.Pin.OUT)
+uart = UART(0, baudrate=57600, bits=8, parity=0, tx=Pin(0), rx=Pin(1))
+while True:
+led.on()
+uart.write("Y")
+utime.sleep(1)
+led.off()
+utime.sleep(1)
